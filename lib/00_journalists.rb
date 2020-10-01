6d016bb110831b7f40556d46ed87740f9b9927a2
Erudigit <duguet.vincent@outlook.fr>
2020-10-01 16:30:51 +0200

    Add 00_journalists.rb

---
+++ lib/00_journalists.rb
@@ -0,0 +1,95 @@
+#VARIABLES
+listing_journalists = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@min","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@ceci_est_un_handle_vraiment_long","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+
+#METHODES
+def print_number_handles(listing_journalists) #Calcul le nombre d'handles dans le tableau d'entrée
+    num = listing_journalists.length
+    puts "Il y a #{num} handles dans ce tableau !"
+    return num
+end
+
+def print_min_handles(listing_journalists) #Affiche l'handle le plus petit
+    shandle = listing_journalists.min_by(&:length)
+    puts "L'handle le plus court est : #{shandle} !"
+    return shandle
+end
+
+def delete_the_at(listing_journalists) #Supprime tous les '@' du tableau d'entrée
+    listing_journalists_without_at = listing_journalists.map {|word| word.gsub('@', '')}
+    return listing_journalists_without_at
+end
+
+def print_number_handles_5char(listing_journalists, num, list_without_at) #Calcul le nombre d'handles à 5 caractères sans compter le @
+    listing_handle_5char = []
+    num.times do |i|
+        if list_without_at[i].length() == 5 then
+            listing_handle_5char << list_without_at[i]
+        end
+    end
+    puts "Il y a #{listing_handle_5char.length} handles de 5 caractères sans le '@' !"
+    return listing_handle_5char
+end
+
+def number_handles_begining_with_cap(listing_journalists, list_without_at, num) #Calcul le nombre d'handles qui commence par une majuscule
+    list_f_letter = []
+    num.times do |i|
+        list_f_letter << list_without_at[i].chars.first
+    end
+    puts "Le nombre d'handles qui commence par une maj est de : #{list_f_letter.count{|e| e =~ /^[A-Z]/}}"
+end
+
+
+def alphabetical_sort(list_without_at) #Trie par ordre alphabétique
+    list_sort_by_alphabet = list_without_at.sort # Ascending sort!
+    #puts "Voici la liste trié par ordre alphabétique : #{list_sort_by_alphabet}"
+end
+
+def length_sort(listing_journalists) #Trie par longueur des handles
+    list_sort_by_length = listing_journalists.sort_by {|x| x.length}
+    #puts "Voici la liste trié par taille des handles : #{list_sort_by_length}"
+end
+
+def print_index_of_epenser(listing_journalists) #Affiche l'index de l'handle "@epenser"
+    index_of_epenser = listing_journalists.index("@epenser")
+    puts "L'index de @epenser est : #{index_of_epenser}"
+end
+
+def sort_by_similar_length(listing_journalists, num) #Créer un nouveau tableau qui affiche une répartition des handle selon leur taille
+    mhandle = listing_journalists.max_by(&:length).length + 1
+    listing_into_length = []
+    listing_into_length_compact = []
+    counter=0
+    num.times do |i| #On transforme le tableau d'entrée en un tableau de nombre de caractère
+        count = 0
+        listing_journalists[i].each_char { |c| count += c.length }
+        #puts count
+        listing_into_length[i] = count
+    end
+    mhandle.times do |e| #Première boucle
+        num.times do |g| #Deuxième boucle
+
+            if listing_into_length[g] == e then
+                counter = counter + 1
+            end
+        end
+        listing_into_length_compact << counter
+        counter = 0
+    end
+    puts listing_into_length_compact
+end
+
+#CALL PROCESS
+def perform(listing_journalists)
+    number_handles = print_number_handles(listing_journalists) #Récupère dans la variable number_handles le nombre d'handle en faisant appel à la fonction print_number_handles
+    list_without_at = delete_the_at(listing_journalists) #Récupère dans la variable list_without_at la liste d'entrée sans les @ en faisant appel à la fonction delete_the_at
+    min_handles = print_min_handles(listing_journalists) #Récupère dans la variable min_handles l'handle le plus petit de la liste d'entrée à l'aide de la fonction print_min_handles
+
+    print_number_handles_5char(listing_journalists, number_handles, list_without_at) #Appel la fonction print_number_handles_5char qui calcul le nombre d'handle composé de 5 caractères
+    number_handles_begining_with_cap(listing_journalists, list_without_at, number_handles) #Appel la fonction number_handles_begining_with_cap qui va compter le nombre d'handles commançant par une majuscule
+    alphabetical_sort(list_without_at) #Appel la fonction alphabetical_sort qui va trier la liste d'entrée par ordre alphabétique
+    length_sort(listing_journalists) #Appel la fonction length_sort qui va trier la liste d'entrée par longueur d'handles
+    print_index_of_epenser(listing_journalists)
+    sort_by_similar_length(listing_journalists, number_handles)
+end
+
+perform(listing_journalists)
